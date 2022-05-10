107129e6f7d45f73de85d73370ad858fecb3bcda
Surya <surya@Emmanuels-MacBook.local>
2022-05-11 02:19:58 +0530

    2nd commit

--- components/Controls.tsx
+++ components/Controls.tsx
@@ -3,34 +3,33 @@ import {
   ArrowCircleUpIcon,
   ArrowCircleLeftIcon,
   ArrowCircleRightIcon,
-} from "@heroicons/react/outline";
+} from '@heroicons/react/outline'
 function Controls() {
-    return (
-
-  <div className="flex absolute inset-x-0 bottom-0 h-1/2 w-5/6 bg-gradient-to-r from-neutral-900 to-black">
-    <hr className="border-t-[0.1px] border-neutral-400" />
-        <h1 className="text-neutral-400 text-center">Controls</h1>
-        <div className="flex-column justify-center grow">
-        <button className="flex items-center justify-center w-64 h-10 bg-neutral-400 rounded-full">
-          <ArrowCircleDownIcon className="h-10 w-10" />
-          <p>Rev</p>
-        </button>
-        <button className=" bottom-14 flex items-center justify-center w-64 h-10 bg-neutral-400 rounded-full">
+  return (
+    <div className="flex-column absolute inset-x-0 bottom-0 h-1/2 w-5/6 bg-gradient-to-r from-neutral-900 to-black">
+      {/* <hr className="border-t-[0.1px] border-neutral-400" /> */}
+      <h1 className="grow-0 text-center text-neutral-400">Controls</h1>
+        <div className="grid gap-4 grid-cols-2 grid-rows-2 justify-content-center h-full first-letter:">
+       
+        <button className="flex h-10 w-64 items-center justify-center m-auto rounded-full grid-row-2 bg-neutral-400">
           <ArrowCircleUpIcon className="h-10 w-10" />
           <p>Fwd</p>
         </button>
-        <button className="flex items-center justify-center w-64 h-10 bg-neutral-400 rounded-full">
+        <button className="flex h-10 w-64 items-center justify-center m-auto rounded-full bg-neutral-400">
           <ArrowCircleLeftIcon className="h-10 w-10" />
           <p>Left</p>
         </button>
-        <button className="flex items-center justify-center w-64 h-10 bg-neutral-400 rounded-full">
+          <button className="flex h-10 w-64 items-center justify-center m-auto rounded-full bg-neutral-400">
+            <ArrowCircleDownIcon className="h-10 w-10" />
+            <p>Rev</p>
+          </button>
+        <button className="flex h-10 w-64 items-center justify-center m-auto rounded-full bg-neutral-400">
           <ArrowCircleRightIcon className="h-10 w-10" />
           <p>Right</p>
         </button>
-        </div>
-  </div>
-
-    )
+      </div>
+    </div>
+  )
 }
 
 export default Controls
--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -3,6 +3,8 @@ function Sidebar() {
 
   <div className="absolute inset-y-0 right-0 w-1/6 bg-gradient-to-r from-neutral-900 to-neutral-900 h-full">
       <h1 className="text-neutral-400 text-center">Sensor Log</h1>
+      <p className="text-neutral-400 text-center text-xs">Human Presence Detected...</p>
+      <p className="text-neutral-400 text-center text-xs">Gas Detected...</p>
   
   </div>
     )
